e953743ddfdabe7f83118121fb916cb497a76d48
Komal <gulati05.komal@gmail.com>
2023-03-12 15:55:02 +0530

    some more changes

--- src/componets/form.jsx
+++ src/componets/form.jsx
@@ -23,31 +23,31 @@ export default function Form() {
       <label>
         First name:
         <input type="text" {...register("firstName", {required: true, maxLength: 80, pattern: /^[a-zA-Z]*$/})} />
-        {errors.firstName && <span className="error-label">Please enter a valid first name</span>}
+        {errors.firstName && <span style="color: red" className="error-label">Please enter a valid first name</span>}
       </label>
       
       <label>
         Last name:
         <input type="text" {...register("lastName", {required: true, maxLength: 100, pattern: /^[a-zA-Z]*$/})} />
-        {errors.lastName && <span className="error-label">Please enter a valid last name</span>}
+        {errors.lastName && <span style="color: red" className="error-label">Please enter a valid last name</span>}
       </label>
       
       <label>
         Email:
         <input type="text" {...register("email", {required: true, pattern: /^\S+@\S+$/i})} />
-        {errors.email && <span className="error-label">Please enter a valid email</span>}
+        {errors.email && <span style="color: red" className="error-label">Please enter a valid email</span>}
       </label>
       
       <label>
         Mobile number:
         <input type="tel" {...register("mobileNumber", {required: true, minLength: 10, maxLength: 12, pattern: /^[0-9]*$/})} />
-        {errors.mobileNumber && <span className="error-label">Please enter a valid mobile number</span>}
+        {errors.mobileNumber && <span style="color: red" className="error-label">Please enter a valid mobile number</span>}
       </label>
       
       <label>
         Date of Birth:
         <input type="date" {...register("dateOfBirth", {required: true})} />
-        {errors.dateOfBirth  && <span className="error-label">Please enter a valid date of birth</span>}
+        {errors.dateOfBirth  && <span style="color: red" className="error-label">Please enter a valid date of birth</span>}
       </label>
 
       <input className="submit-button" type="submit" value="Submit" />
